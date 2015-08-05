ee77af3b61562688f6c8e1ab01a5851d40038b81
Cindy <cindy.leschaud@gmail.com>
2015-08-05 15:08:43 +0200

    Revert to 83510174b484f30bf23fb6eb1791f380ba9403f7

--- resources/views/oauth/authorization-form.blade.php
+++ resources/views/oauth/authorization-form.blade.php
@@ -2,12 +2,14 @@
 @section('content')
 
     {!! Form::open(['method' => 'POST','class'=>'form-horizontal', 'url'=> route('oauth.authorize.post',$params)]) !!}
+
     <div class="form-group">
         <dl class="dl-horizontal">
             <dt>Client Name</dt>
-            <dd>{{$client->getName()}}</dd>
+            <dd>{{ $client->getName() }}</dd>
         </dl>
     </div>
+
     {!! Form::hidden('client_id', $params['client_id']) !!}
     {!! Form::hidden('redirect_uri', $params['redirect_uri']) !!}
     {!! Form::hidden('response_type', $params['response_type']) !!}
